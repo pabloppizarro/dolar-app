9daef797ff00afcfdf9c9ec4e47170a38989d522
pabloppizarro <pizarro.pablo@outlook.com>
2023-11-03 15:10:20 -0300

    added parallel react queries with tanstack library

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,52 +3,61 @@ import Card from "../components/Card";
 import CurrencyConverter from "../components/CurrencyConverter";
 import LiteCard from "../components/LiteCard";
 
+import { useQuery } from "@tanstack/react-query";
 export default function Home() {
-  const [isLoading, setLoading] = useState(true);
-  const [usd, setUsd] = useState();
-  const [usdOficial, setUsdOficial] = useState();
-  const [laNacionUSD, setLaNacionUSD] = useState([]);
-  useEffect(() => {
-    const promises = Promise.all([
-      fetch(`${process.env.REACT_APP_API}/blue-dolarhoy`).catch((err) => {}),
-      fetch(`${process.env.REACT_APP_API}/oficial-dolarhoy`).catch((err) => {}),
-      fetch(`${process.env.REACT_APP_API}/lanacion`).catch((err) => []),
-    ]);
-    promises.then(async (values) => {
-      const blue = await values[0].json();
-      const oficial = await values[1].json();
-      setLaNacionUSD(await values[2].json());
-      setUsd(blue);
-      setUsdOficial(oficial);
-      setLoading(false);
-    });
-  }, []);
+  // const [isLoading, setLoading] = useState(false);
+
+  const nacionQuery = useQuery({
+    queryKey: ["lanacion"],
+    queryFn: fetchDolar,
+  });
+  const blueQuery = useQuery({
+    queryKey: ["blue-dolarhoy"],
+    queryFn: fetchDolar,
+  });
+  const oficialQuery = useQuery({
+    queryKey: ["oficial-dolarhoy"],
+    queryFn: fetchDolar,
+  });
 
   return (
     <div className="mx-4 mt-4 grid gap-4">
       <h1 className=" text-2xl">DÓLAR DEL MOMENTO</h1>
-      {isLoading ? (
-        <section>
-          <h1>Loading</h1>
+      <div className="grid  grid-cols-1 gap-2  md:grid-cols-2">
+        <section className="flex w-full flex-col  gap-2 ">
+          <Card
+            data={blueQuery.data}
+            isError={blueQuery.isError}
+            isLoading={blueQuery.isLoading}
+          />
+          <Card
+            data={oficialQuery.data}
+            isError={oficialQuery.isError}
+            isLoading={oficialQuery.isLoading}
+          />
+          {/* <Card props={oficialDolarHoyQuery} /> */}
         </section>
-      ) : (
-        <>
-          <div className="grid  grid-cols-1 gap-2  md:grid-cols-2">
-            <section className="flex w-full flex-col  gap-2 ">
-              <Card props={usd} />
-              <Card props={usdOficial} />
-            </section>
-            <section className="grid">
-              {laNacionUSD.map((item, i) => (
+        <section className="grid">
+          {nacionQuery.isLoading ? <p className="m-auto">loading...</p> : ""}
+          {nacionQuery.isError ? <p>Error en servicio La Nación.</p> : ""}
+          {nacionQuery.isSuccess
+            ? nacionQuery.data.map((item, i) => (
                 <LiteCard props={item} key={i}></LiteCard>
-              ))}
-            </section>
-          </div>
-          <section>
-            <CurrencyConverter props={usd} />
-          </section>
-        </>
-      )}
+              ))
+            : ""}
+        </section>
+      </div>
+      <section>
+        {blueQuery.data ? <CurrencyConverter props={blueQuery.data} /> : ""}
+      </section>
     </div>
   );
+}
+
+async function fetchDolar({ queryKey }) {
+  const response = await fetch(`${process.env.REACT_APP_API}/${queryKey}`);
+  if (!response.ok) {
+    throw new Error("Error en llamado a datos.");
+  }
+  return response.json();
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,40 @@
 import { useId } from "react";
 
-export default function Card({ props }) {
-  // const id = useId();
+export default function Card({ data, isLoading, isError }) {
   return (
     <article className="currency-card mx-auto my-0">
-      <div>
-        <h3 className=" text-lg font-bold">{props.title}</h3>
-        <small className=" m-auto rounded-xl bg-amber-100 p-1">
-          {props.house}
-        </small>
-      </div>
+      {data ? (
+        <>
+          <div>
+            <h3 className=" text-lg font-bold">{data.title}</h3>
+            <small className=" m-auto rounded-xl bg-amber-100 p-1">
+              {data.house}
+            </small>
+          </div>
 
-      <div className="grid grid-cols-2 justify-center gap-2">
-        <div className="rounded-lg border p-2">
-          <h3 className=" content object-fill text-xl">COMPRA</h3>
-          <p className="text-2xl font-bold tabular-nums"> {`$${props.buy}`}</p>
-        </div>
-        <div className="rounded-lg border p-2">
-          <h3 className=" text-xl">VENTA</h3>
-          <p className="text-2xl font-bold  tabular-nums">{`$${props.sell}`}</p>
-        </div>
-      </div>
-      <div>
-        <p className=" rounded-full bg-amber-300 font-poppinsBold">
-          <small>últ. act: </small>
-          {props.date}
-        </p>
-      </div>
+          <div className="grid grid-cols-2 justify-center gap-2">
+            <div className="rounded-lg border p-2">
+              <h3 className=" content object-fill text-xl">COMPRA</h3>
+              <p className="text-2xl font-bold tabular-nums">
+                {" "}
+                {`$${data.buy}`}
+              </p>
+            </div>
+            <div className="rounded-lg border p-2">
+              <h3 className=" text-xl">VENTA</h3>
+              <p className="text-2xl font-bold  tabular-nums">{`$${data.sell}`}</p>
+            </div>
+          </div>
+          <div>
+            <p className=" rounded-full bg-amber-300 font-poppinsBold">
+              <small>últ. act: </small>
+              {data.date}
+            </p>
+          </div>
+        </>
+      ) : null}
+      {isLoading ? <p className=" m-auto">Cargando...</p> : ""}
+      {isError ? <p>No hay datos..</p> : ""}
     </article>
   );
 }
